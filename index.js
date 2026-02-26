9bc3d6e548433235564ed9edb0cedcee0b91614e
prashanthreddy2004 <prashanthreddy3107@gmail.com>
2026-02-26 17:56:01 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path=require('path')
+const pug=require('pug');
+
+var app=express();
+
+app.set('views',path.join(__dirname,"views"));
+app.set("view engine ","pug");
+
+app.get('/home',(req,res)=>{
+    const subjects =["Devops","cc","Cd"];
+    res.render('sample.pug',{'sub': subjects});
+
+});
+
+app.listen(4000,()=>{
+    console.log("SERVER SUCCESSFULLY");
+});
